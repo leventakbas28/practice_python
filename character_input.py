d2fc3b9f295ec3a26cb685981c71ed0a4ef2f27a
Akbas <akbasl@amazon.lu>
2022-12-16 12:38:27 +0100

    initial commit

---
+++ character_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = int(input("What is your age?"))
+year = 2022 + (100 - age)
+print("You will 100 years old at {year}".format(year = year))
---
+++ odd_or_even.py
@@ -0,0 +1,17 @@
+number = int(input("Please enter a number: "))
+if (number % 4 == 0):
+    print("The number is 4X.")
+elif (number % 2 == 0):
+    print("The number is EVEN.")
+else:
+    print("The number is ODD.")
+
+num = int(input("Please enter the number: "))
+check = int(input("Please enter the check: "))
+
+if (num % check == 0):
+    print("Divides evenly")
+else:
+    print("Not divides evenly")
+
+
