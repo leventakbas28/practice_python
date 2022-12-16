4efb0c67ab71b59a289c90498993c5d09c1f70f0
Akbas <akbasl@amazon.lu>
2022-12-16 12:50:59 +0100

    second commit

--- character_input.py
+++ character_input.py
@@ -1,4 +1,7 @@
-name = input("What is your name?")
-age = int(input("What is your age?"))
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+repeat = int(input("How many repeats? "))
 year = 2022 + (100 - age)
-print("You will 100 years old at {year}".format(year = year))
+while (repeat > 0):
+    print("You will 100 years old at {year}".format(year = year))
+    repeat -= 1
