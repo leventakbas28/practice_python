773b08f17fc8ce7a6bc0bccf1f59440a79dfc41c
Akbas <akbasl@amazon.lu>
2022-12-18 22:14:44 +0100

    second commit

--- list_less_than_ten.py
+++ list_less_than_ten.py
@@ -1,7 +1,10 @@
 initial_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+new_list = [number for number in initial_list if number < 5]
+print(new_list)
 temp_list = []
 check = int(input("Please enter check value: "))
 for number in initial_list:
     if number < check:
         temp_list.append(number)
 print(temp_list)
+
