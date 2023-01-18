d292f0cff32bf27615680f269946ebc354bea79b
Akbas <akbasl@amazon.lu>
2023-01-19 00:25:15 +0100

    fourth commit

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -1,20 +1,7 @@
 import random
+guess_count = 0
 
-def compare(input):
-    random_number = str(random.randint(1000, 9999))
-    #random_number = "1234"
-    cow = 0
-    bull = 0
-    for i in range(4):
-        if random_number[i] == input[i]:
-            cow += 1
-        else:
-            bull += 1
-    #print(random_number)
-    print("{cow} cows, {bull} bulls".format(cow = cow, bull = bull))
-    return cow
-
-while True:
+def get_input():
     while True:
         user_input = input("Please guess the number: ") 
         if user_input.isdigit():
@@ -26,12 +13,30 @@ while True:
         else:
             print("Wrong input. Try again!")
             continue
+    return user_input
+
+def compare(input):
+    random_number = str(random.randint(1000, 9999))
+    #random_number = "1234"
+    cow = 0
+    bull = 0
+    for i in range(4):
+        if random_number[i] == input[i]:
+            cow += 1
+        else:
+            bull += 1
+    print("{cow} cows, {bull} bulls".format(cow = cow, bull = bull))
+    return cow
+
+while True:
+    user_input = get_input()
+    guess_count += 1
     cows = compare(user_input)
     if cows == 4:
-        print("Success!")
+        print("Guess number: {guess_count} --> Success!".format(guess_count = guess_count))
         break
     else:
-        run_input = input("Try again or exit? ").lower()
+        run_input = input("Guess number: {guess_count} --> Try again or exit? ".format(guess_count = guess_count)).lower()
         if run_input == "exit":
             break
         else:
